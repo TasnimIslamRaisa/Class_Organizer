dde59defc11906c64ec7ef60f45e78a96deff275
fahradfoysal <edu.farhad.foysal@gmail.com>
2024-07-16 18:46:34 +0600

    db, models, onboardin added by farhad foysal 1

--- class_organizer/lib/main.dart
+++ class_organizer/lib/main.dart
@@ -4,10 +4,10 @@ import 'class_organizer_app.dart';
 import 'db/database_manager.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final dbManager = DatabaseManager();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final dbManager = DatabaseManager();
 
-  // Open the database
-  final db = await dbManager.database;
+  // // Open the database
+  // final db = await dbManager.database;
   runApp(const ClassOrganizerApp());
 }
