cad815d422844c2918c36a3265d8478683615b9d
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-09-07 03:11:25 +0600

    used getx in opt

--- class_organizer/lib/class_organizer_app.dart
+++ class_organizer/lib/class_organizer_app.dart
@@ -1,22 +1,18 @@
 import 'package:class_organizer/splash/splash_screen_v1.dart';
-import 'package:class_organizer/style/themes/lightTheme.dart';
-import 'package:class_organizer/style/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:provider/provider.dart';
 class ClassOrganizerApp extends StatelessWidget {
   const ClassOrganizerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Class Organizer',
       debugShowCheckedModeBanner:false ,
-      theme:  Provider.of<ThemeProvider>(context).themeData,
+      theme: Provider.of(context).ThemeData,
       home: const SplashScreenV1(),
 
     );
   }
-
-
-
 }
--- class_organizer/lib/ui/screens/auth/otp_verification_screen.dart
+++ class_organizer/lib/ui/screens/auth/otp_verification_screen.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 import 'package:class_organizer/ui/screens/auth/set_password_screen.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import '../../../style/app_color.dart';
 import '../../widgets/background_widget.dart';
+import '../controller/otp_controller.dart';
 import 'SignInScreen.dart';
 
 class OtpVerificationScreen extends StatefulWidget {
@@ -15,7 +17,9 @@ class OtpVerificationScreen extends StatefulWidget {
 }
 
 class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
-  final TextEditingController textEditingController = TextEditingController();
+  final TextEditingController _otpController = TextEditingController();
+  final OTPController otpController = Get.put(OTPController());
+
   StreamController<ErrorAnimationType> errorController = StreamController<ErrorAnimationType>();
 
   @override
@@ -63,7 +67,7 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
                     backgroundColor: Colors.transparent,
                     enableActiveFill: true,
                     errorAnimationController: errorController,
-                    controller: textEditingController,
+                    controller: _otpController,
                     appContext: context,
                   ),
                   const SizedBox(
@@ -73,30 +77,37 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
                     onPressed: onTabVerifyButton,
                     child: const Text("Verify",),
                   ),
-                  const SizedBox(
-                    height: 45,
-                  ),
-                  Center(
+                  const SizedBox( height: 45,),
+                  Obx(()=>Center(
                     child: RichText(
-                      text: TextSpan(
-                        style: const TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.textColor,
-                            letterSpacing: 0.4),
-                        text: "Have an account?",
-                        children: [
-                          TextSpan(
-                            text: "  Sign-In",
-                            style: const TextStyle(
-                              color: AppColors.primaryColor,
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = onTabSignInButton,
-                          ),
-                        ],
-                      ),
+                        text: TextSpan(
+                            style: Theme.of(context).textTheme.bodyLarge,
+                            text: "This code will expire in : ",
+                            children:  [
+                              TextSpan(
+                                  text: '${otpController.countdown.value}s',
+
+                                  style:const TextStyle(
+                                    color: Colors.lightBlueAccent,
+                                  )
+                              ),
+                            ]
+                        )
                     ),
+                  )),
+                  const SizedBox(height: 6,),
+                  Obx(
+                        () => Center(
+                          child: TextButton(
+                                                onPressed: otpController.isResendEnabled.value
+                            ? otpController.resendCode
+                            : null,
+                                                child: const Text("Resend Code",
+                            style:const TextStyle(
+                          color: Colors.lightBlueAccent,
+                                                )),
+                                              ),
+                        ),
                   ),
                 ],
               ),
@@ -130,7 +141,7 @@ class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
 
   @override
   void dispose() {
-    textEditingController.dispose();
+    _otpController.dispose();
     errorController.close(); // Close the StreamController to prevent memory leaks
     super.dispose();
   }
