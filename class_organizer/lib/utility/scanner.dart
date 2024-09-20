bea6bca37427303c32f90089eb6ff4d38b10e93b
fahradfoysal <edu.farhad.foysal@gmail.com>
2024-09-20 18:10:35 +0600

    scanner

---
+++ class_organizer/lib/utility/scanner.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'download_page.dart';
+
+class Scanner extends StatefulWidget {
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  String scannedCode = '';
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (controller != null) {
+      controller!.pauseCamera();
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR/Barcode Scanner'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (scannedCode.isNotEmpty)
+                  ? ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          DownloadPage(scannedCode: scannedCode),
+                    ),
+                  );
+                },
+                child: Text('Go to Download Page'),
+              )
+                  : Text('Scan a code'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController qrController) {
+    this.controller = qrController;
+    qrController.scannedDataStream.listen((scanData) {
+      setState(() {
+        scannedCode = scanData.code ?? '';
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
